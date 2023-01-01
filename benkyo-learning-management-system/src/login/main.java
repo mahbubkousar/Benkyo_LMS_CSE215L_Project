83d96854bc10b0acc2ffccc34af0a2732a574624
Mahbub <mahbubkousar@gmail.com>
2023-01-01 18:31:52 +0600

    final commit

---
+++ benkyo-learning-management-system/src/login/main.java
@@ -0,0 +1,24 @@
+package login;
+
+import javafx.application.Application;
+import static javafx.application.Application.launch;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class main extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/login/Login.fxml"));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
